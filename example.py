06c1787272beae8d09707a321984d3771245a24f
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-30 19:29:10 -0400

    fix for py34

--- example.py
+++ example.py
@@ -5,4 +5,4 @@ from pyjarowinkler import distance
 if __name__ == "__main__":
     first = "hello"
     second = "haloa"
-    print "The words '{0}' and '{1}' matches at {2}%".format(first, second, distance.get_jaro_distance(first, second))
+    print("The words '{0}' and '{1}' matches at {2}%".format(first, second, distance.get_jaro_distance(first, second)))

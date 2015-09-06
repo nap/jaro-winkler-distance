52a224865079b9b9106390650bdc4d676873a5d1
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-09-06 16:15:32 -0400

    update release script with automatic changelog update

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! git diff-index --quiet HEAD; then
+	echo "Can not process, there's uncommited changes."
+	exit 1
+fi
+
 if echo "$@" | grep -q -- '--version'; then
 	VERSION="$(echo "$@" | grep -oh -- '--version=.*' | cut -d '=' -f 2)"
 fi
@@ -9,18 +14,27 @@ if echo "$@" | grep -q -- '--identity'; then
 fi
 [[ -z "$IDENTITY" ]] && IDENTITY=E80E2315
 
+CURRENT_VERSION=$(sed -n "s/__version__ = '\(.*\)'/\1/p" setup.py)
 if [[ -z "$VERSION" ]]; then
 	echo "Usage: $(basename $0) --version=VERSION [--identity=IDENTITY]"
-	echo " - Current version: $(sed -n "s/__version__ = '\(.*\)'/\1/p" setup.py)"
+	echo " - Current version: $CURRENT_VERSION"
 	exit 1
 fi
 
 DATE=$(date +%Y-%m-%d)
+CHANGELOG=$(mktemp -t tmp)
+echo "v$VERSION (${DATE})" > $CHANGELOG
+echo "$(git log v${CURRENT_VERSION}..HEAD --oneline | egrep -v "bump|pypi|pep8")" | while read LINE; do
+	echo -e "    $LINE" >> $CHANGELOG
+done
+cat CHANGELOG >> $CHANGELOG
+cat $CHANGELOG > CHANGELOG
+
 sed -i '' "s/__version__ = '.*'/__version__ = '$VERSION'/" setup.py
 sed -i '' "s/:Version: .*/:Version: $VERSION of $DATE/" README.rst
 
-git add README.rst setup.sh
-git commit -m "version bump"
+git add README.rst setup.sh CHANGELOG
+git commit -m "version bump and changelog update"
 git push
 
 rm -Rf dist/*

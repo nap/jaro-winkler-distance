836fec0c80e754fd9a01a1221407ceeda419188e
GustavoPeredo <gustavomperedo@protonmail.com>
2021-07-04 17:18:46 -0300

    Fix names in distance.py

--- pyjarowinkler/distance.py
+++ pyjarowinkler/distance.py
@@ -49,7 +49,7 @@ def get_jaro_distance_array(first, second, winkler=True, winkler_ajustment=True,
     """
     if not instanceof(second, list):
         second = [second]
-    return [get_jaro_distance(string, i, winkler, winkler_ajustment, scaling) for i in array]
+    return [get_jaro_distance(first, i, winkler, winkler_ajustment, scaling) for i in second]
 
 
 def _score(first, second):
